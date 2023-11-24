3653283725790125e546769f7dd4e6ccb9a90758
Ivylou <princequincyy@gmail.com>
2023-11-25 01:18:11 +0300

    crud comment n new endpoint

--- app/schemas/comment.py
+++ app/schemas/comment.py
@@ -1,6 +1,38 @@
-from typing import List
+from typing import List, Optional
+from datetime import datetime
 
 from pydantic import BaseModel, ConfigDict
 
+from app.schemas.users import UserOut
 
-class Comment(BaseModel): pass
+
+class CommentCreate(BaseModel):
+	text: str
+	parent_comment_id: int | None = None
+
+
+class CommentUpdate(BaseModel):
+	text: str
+
+
+class CommentDBCreate(CommentCreate):
+	model_config = ConfigDict(from_attributes=True)
+
+	created_at: datetime | None = None
+	updated_at: datetime | None = None
+	user_id: int
+
+
+class CommentDBUpdate(CommentUpdate):
+	updated_at: datetime
+
+
+class CommentDBOut(CommentDBCreate):
+	id: int
+	author: UserOut | None = None
+	parent_comment: Optional["CommentDBOut"] = None
+	child_comments: Optional[List["CommentDBOut"]] = None
+	commentable_type: str
+	commentable_id: int
+
+
