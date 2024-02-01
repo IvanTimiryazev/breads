d23d95cddc8eecc427dea6de11fb2a8d315fbce3
Ivylou <princequincyy@gmail.com>
2024-02-01 17:22:43 +0300

    image module refactoring

--- app/api/api_v1/endpoints/image.py
+++ app/api/api_v1/endpoints/image.py
@@ -10,7 +10,8 @@ from sqlalchemy import select
 from app.api.deps import get_db, get_current_user
 from app.crud.crud_comment import comment
 from app.schemas import image
-from app.schemas.comment import CommentDBOut, CommentCreate, CommentDBCreate
+from app.schemas.comment import CommentDBOut, CommentCreate, CommentDBCreate, CommentUpdate, CommentDBUpdate, \
+	CommentsDBOut, CommentDBOutWithComments
 from app.schemas.exceptions import ErrorResponse
 from app.schemas.responses import SuccessResponse
 from app.models.users import Users
@@ -60,59 +61,115 @@ def get_images(
 		current_user: Annotated[Users, Depends(get_current_user)]
 ) -> Any:
 	_url = Path(f"{settings.SERVER_HOST}:{settings.SERVER_PORT}/static/images")
-	images_out = [image.ImageOut(filepath=_url/i.name, name=i.name) for i in current_user.images.all()]
-	return {"images": images_out}
+	_images_out = [
+		image.ImageOut(**image.ImageDBOut.model_validate(i).model_dump(), filepath=_url / i.name) for i in
+		current_user.images.all()
+	]
+	return {"images": _images_out}
 
 
-@router.delete("/image/{filename}", response_model=SuccessResponse, status_code=status.HTTP_200_OK)
+@router.delete("/image/{image_id}", response_model=SuccessResponse, status_code=status.HTTP_200_OK)
 def delete_image(
 		*,
 		db: Annotated[Session, Depends(get_db)],
 		current_user: Annotated[Users, Depends(get_current_user)],
-		filename: str
+		image_id: int
 ) -> Any:
-	image_delete(filename)
-	db_image = db.execute(select(Image).filter_by(name=filename)).scalar_one_or_none()
+	db_image = db.execute(select(Image).filter_by(id=image_id)).scalar_one_or_none()
+	image_delete(db_image.name)
 	current_user.images.remove(db_image)
 	db.commit()
 	return {"success": "Image has been deleted."}
 
 
-@router.get("/download/{filename}", status_code=status.HTTP_200_OK)
+@router.get("/download/{image_id}", status_code=status.HTTP_200_OK)
 def download_image(
 		*,
+		db: Annotated[Session, Depends(get_db)],
 		current_user: Annotated[Users, Depends(get_current_user)],
-		filename: str
+		image_id: int
 ) -> FileResponse:
-	filepath = image_exist_check(filename)
+	db_image = db.execute(select(Image).filter_by(id=image_id)).scalar_one_or_none()
+	filepath = image_exist_check(db_image.name)
 	return FileResponse(filepath)
 
 
-@router.post("/{filename}/comment", response_model=CommentDBOut, status_code=status.HTTP_201_CREATED)
+@router.post("/{image_id}/comment", response_model=CommentDBOut, status_code=status.HTTP_201_CREATED)
 def create_comment(
 		*,
 		db: Annotated[Session, Depends(get_db)],
 		current_user: Annotated[Users, Depends(get_current_user)],
-		filename: str,
+		image_id: int,
 		obj_in: CommentCreate
 ) -> Any:
 	"""Создает комментарий к фото."""
-	filepath = image_exist_check(filename)
-	db_image = db.execute(select(Image).filter_by(name=filename)).scalar_one_or_none()
+	db_image = db.execute(select(Image).filter_by(id=image_id)).scalar_one_or_none()
+	filepath = image_exist_check(db_image.name)
 	comment_obj = CommentDBCreate(**obj_in.model_dump(), user_id=current_user.id)
 	db_comment = comment.create(db, obj_in=comment_obj, obj_to_comment=db_image)
 	return db_comment
 
 
-@router.get("/{filename}", response_model=image.ImageOut, status_code=status.HTTP_200_OK)
+@router.put("/{image_id}/comment/{comment_id}", response_model=CommentDBOut, status_code=status.HTTP_200_OK)
+def update_comment(
+		*,
+		db: Annotated[Session, Depends(get_db)],
+		current_user: Annotated[Users, Depends(get_current_user)],
+		image_id: int,
+		comment_id: int,
+		obj_in: CommentUpdate
+) -> Any:
+	"""Редактирует комментарий. (Текст)"""
+	db_image = db.execute(select(Image).filter_by(id=image_id)).scalar_one_or_none()
+	db_comment = comment.get(db, id_=comment_id)
+	comment_obj = CommentDBUpdate(**obj_in.model_dump(), updated_at=datetime.utcnow())
+	db_comment = comment.update(db, db_obj=db_comment, obj_in=comment_obj)
+	return db_comment
+
+
+@router.delete("/{image_id}/comment/{comment_id}", response_model=SuccessResponse, status_code=status.HTTP_200_OK)
+def delete_comment(
+		*,
+		db: Annotated[Session, Depends(get_db)],
+		current_user: Annotated[Users, Depends(get_current_user)],
+		comment_id: int
+) -> Any:
+	comment.remove(db, id_=comment_id)
+	return {"success": "Comment has been deleted."}
+
+
+@router.get("/{image_id}/comments", response_model=CommentsDBOut, status_code=status.HTTP_200_OK)
+def get_comments(
+		*,
+		db: Annotated[Session, Depends(get_db)],
+		current_user: Annotated[Users, Depends(get_current_user)],
+		image_id: int
+) -> Any:
+	db_image = db.execute(select(Image).filter_by(id=image_id)).scalar_one_or_none()
+	db_comments = comment.get_object_comments(db, obj_to_comment=db_image)
+	return {"comments": db_comments}
+
+
+@router.get("/comment/{comment_id}", response_model=CommentDBOutWithComments, status_code=status.HTTP_200_OK)
+def get_comment(
+		*,
+		db: Annotated[Session, Depends(get_db)],
+		current_user: Annotated[Users, Depends(get_current_user)],
+		comment_id: int
+) -> Any:
+	"""Возвращает комментарий по id"""
+	db_comment = comment.get(db, id_=comment_id)
+	return db_comment
+
+
+@router.get("/{image_id}", response_model=image.ImageOut, status_code=status.HTTP_200_OK)
 def get_image(
 		*,
 		db: Annotated[Session, Depends(get_db)],
 		current_user: Annotated[Users, Depends(get_current_user)],
-		filename: str
+		image_id: int
 ) -> Any:
-	filepath = image_exist_check(filename)
-	db_image = db.execute(select(Image).filter_by(name=filename)).scalar_one_or_none()
+	db_image = db.execute(select(Image).filter_by(id=image_id)).scalar_one_or_none()
 	if not db_image:
 		error_response = ErrorResponse(
 			loc="filename",
@@ -123,7 +180,8 @@ def get_image(
 			status_code=status.HTTP_404_NOT_FOUND,
 			detail=[error_response.model_dump()]
 		)
-	url = f"{settings.SERVER_HOST}:{settings.SERVER_PORT}/static/images/{filename}"
+	filepath = image_exist_check(db_image.name)
+	url = f"{settings.SERVER_HOST}:{settings.SERVER_PORT}/static/images/{db_image.name}"
 	_image = image.ImageDBOut.model_validate(db_image)
 	_image = _image.model_dump()
 	_image["filepath"] = url
